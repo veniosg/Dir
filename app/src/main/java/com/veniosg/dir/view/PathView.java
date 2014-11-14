86c78a2225bfa4e62b7ff2ebe06d0576ce4bae69
George Venios <veniosg@gmail.com>
2014-11-14 21:18:06 +0000

    Progress on visuals and functionality on PathView

--- app/src/main/java/com/veniosg/dir/view/PathView.java
+++ app/src/main/java/com/veniosg/dir/view/PathView.java
@@ -17,20 +17,32 @@
 package com.veniosg.dir.view;
 
 import android.content.Context;
+import android.text.InputType;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toolbar;
 import android.widget.ViewFlipper;
 
 import com.veniosg.dir.R;
+import com.veniosg.dir.util.Logger;
 
 import java.io.File;
 
+import static android.content.Context.INPUT_METHOD_SERVICE;
 import static android.os.Environment.getExternalStorageDirectory;
+import static android.text.InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS;
+import static android.text.InputType.TYPE_TEXT_VARIATION_URI;
+import static android.view.KeyEvent.ACTION_DOWN;
+import static android.view.KeyEvent.KEYCODE_DPAD_CENTER;
 import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
+import static android.view.inputmethod.EditorInfo.IME_ACTION_GO;
 import static com.veniosg.dir.util.FileUtils.isOk;
 import static com.veniosg.dir.util.Utils.backWillExit;
 import static com.veniosg.dir.util.Utils.lastCommonDirectoryIndex;
@@ -44,13 +56,34 @@ public class PathView extends ViewFlipper implements PathController {
     private Mode mCurrentMode = STANDARD_INPUT;
     private File mCurrentDirectory = getExternalStorageDirectory();
     private File mInitialDirectory = getExternalStorageDirectory();
-    private OnDirectoryChangedListener mDirectoryChangedListener = noOpOnDirectoryChangedListener();
 
     private ViewGroup mPathContainer;
     private View mButtonRight;
     private View mManualButtonLeft;
     private View mManualButtonRight;
     private EditText mManualText;
+
+    private OnDirectoryChangedListener mDirectoryChangedListener = noOpOnDirectoryChangedListener();
+    private final OnClickListener mSwitchToManualOnClickListener = new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switchToManualInput();
+        }
+    };
+    private final OnClickListener mResetManualInputClickListener = new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            String path = mCurrentDirectory.getAbsolutePath();
+            setManualInputPath(path);
+        }
+    };
+
+    private final OnClickListener mApplyManualInputClickListener = new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            manualInputCd(mManualText.getText().toString());
+        }
+    };
 
     public PathView(Context context) {
         super(context);
@@ -72,6 +105,28 @@ public class PathView extends ViewFlipper implements PathController {
         mManualButtonLeft = findViewById(R.id.pathview_manual_button_left);
         mManualButtonRight = findViewById(R.id.pathview_manual_button_right);
         mManualText = (EditText) findViewById(R.id.pathview_manual_text);
+
+        mButtonRight.setOnClickListener(mSwitchToManualOnClickListener);
+        mManualButtonLeft.setOnClickListener(mResetManualInputClickListener);
+        mManualButtonRight.setOnClickListener(mApplyManualInputClickListener);
+        mManualText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if ((actionId == IME_ACTION_GO)
+                        || ((event.getAction() == ACTION_DOWN) && ((event.getKeyCode() == KEYCODE_DPAD_CENTER) || (event.getKeyCode() == KeyEvent.KEYCODE_ENTER)))) {
+                    if (manualInputCd(v.getText().toString()))
+                        // Since we have successfully navigated.
+                        return true;
+                }
+
+                // We can't focus the text again so just hide the damned thing.
+                hideKeyboard();
+                return false;
+            }
+        });
+
+        // XML doesn't always work
+        mManualText.setInputType(TYPE_TEXT_VARIATION_URI | TYPE_TEXT_FLAG_NO_SUGGESTIONS);
     }
 
     @Override
@@ -103,7 +158,7 @@ public class PathView extends ViewFlipper implements PathController {
 
     @Override
     public boolean cd(File file, boolean forceNoAnim) {
-        boolean res = false;
+        boolean result = false;
 
         if (isOk(file)) {
             File oldDir = new File(mCurrentDirectory.getAbsolutePath());
@@ -111,17 +166,18 @@ public class PathView extends ViewFlipper implements PathController {
 
             updateViews(forceNoAnim ? null : oldDir, mCurrentDirectory);
 
-            res = true;
-        } else
-            res = false;
+            result = true;
+        } else {
+            result = false;
+        }
 
         mDirectoryChangedListener.directoryChanged(file);
 
-        return res;
+        return result;
     }
 
     private void updateViews(File previousDir, File newDir) {
-        mManualText.setText(newDir.getAbsolutePath());
+        setManualInputPath(newDir.getAbsolutePath());
         updatePathContainer(previousDir, newDir);
     }
 
@@ -132,13 +188,13 @@ public class PathView extends ViewFlipper implements PathController {
     private void updatePathContainer(File p, File n) {
         // Remove only the non-matching buttons.
         int lastCommonDirectory;
-        if(p != null && getChildCount() > 0) {
+        if(p != null && mPathContainer.getChildCount() > 0) {
             lastCommonDirectory = lastCommonDirectoryIndex(p, n);
         } else {
             // First layout, init by hand.
             lastCommonDirectory = -1;
         }
-        for (int i = getChildCount()-1; i > lastCommonDirectory; i--) {
+        for (int i = mPathContainer.getChildCount()-1; i > lastCommonDirectory; i--) {
             mPathContainer.removeViewAt(i);
         }
 
@@ -164,8 +220,9 @@ public class PathView extends ViewFlipper implements PathController {
                 if (cDir++ >= firstDirToAdd) {
                     // Add a button
                     mPathContainer.addView(newButton(cPath.toString(), this));
-                    if(firstDirToAdd != 0) // if not on first draw
-                        mPathContainer.getChildAt(mPathContainer.getChildCount() - 1).setAlpha(0); // So that it doesn't flash due to the animation's delay
+                    // TODO uncomment
+//                    if(firstDirToAdd != 0) // if not on first draw
+//                        mPathContainer.getChildAt(mPathContainer.getChildCount() - 1).setAlpha(0); // So that it doesn't flash due to the animation's delay
                 }
             }
         }
@@ -228,6 +285,28 @@ public class PathView extends ViewFlipper implements PathController {
     @Override
     public void setEnabled(boolean enabled) {
         super.setEnabled(enabled);
+    }
+
+    private boolean manualInputCd(String path) {
+        if (cd(path)) {
+            hideKeyboard();
+            switchToStandardInput();
+            return true;
+        } else {
+            Logger.log(Log.WARN, Logger.TAG_PATHBAR, "Input path does not exist or is not a folder!");
+            return false;
+        }
+    }
+
+    private void hideKeyboard() {
+        InputMethodManager imm = (InputMethodManager) getContext()
+                .getSystemService(INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(getWindowToken(), 0);
+    }
+
+    private void setManualInputPath(String path) {
+        mManualText.setText(path);
+        mManualText.setSelection(path.length());
     }
 
     private Toolbar.LayoutParams toolbarLayoutParams() {
