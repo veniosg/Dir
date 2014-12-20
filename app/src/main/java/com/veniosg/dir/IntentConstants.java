e9cec154ac834fbf7e3bd7ff5281f0ef8bb36205
George Venios <veniosg@gmail.com>
2014-12-20 11:02:39 +0000

    Added Copyright notices to files

--- app/src/main/java/com/veniosg/dir/IntentConstants.java
+++ app/src/main/java/com/veniosg/dir/IntentConstants.java
@@ -1,5 +1,6 @@
 /* 
  * Copyright (C) 2008 OpenIntents.org
+ * Copyright (C) 2014 George Venios
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
